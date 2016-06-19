05616e9048e70da3e036e60595602ea6f49f6698
i5ting <i5ting@126.com>
2016-06-19 14:12:41 +0800

    lazy push commmit, please ignore

--- test.js
+++ test.js
@@ -19,7 +19,6 @@ test.cb('my exec.js', t => {
     exec('node exec.js', function (error, stdout, stderr) {
         t.ifError(error)
         t.regex(stdout, /i5ting-hello-node/)
-        t.fail()
         t.end()
     })
 })
